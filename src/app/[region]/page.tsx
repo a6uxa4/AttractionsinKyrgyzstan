cbf65fbf0989d047c0e9636051f1f3a8cfbeb661
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-06-12 08:55:49 +0600

    fixed

--- src/app/[region]/page.tsx
+++ src/app/[region]/page.tsx
@@ -4,11 +4,10 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCreative } from "swiper/modules";
 import { usePathname } from "next/navigation";
 import { GENERATE_DATA } from "@/utils/constants";
+import path from "path";
 
 export default function Home() {
   const pathname = usePathname();
-
-  console.log(pathname.slice(1));
 
   return (
     <main className="w-full max-w-[1440px] flex items-center justify-center min-h-[calc(100vh_-_50px)]">
@@ -29,7 +28,9 @@ export default function Home() {
         {GENERATE_DATA[pathname.slice(1)].map((item, index) => (
           <SwiperSlide key={index}>
             <div
-              className={`w-full h-full rounded-md relative bg-${item?.img}`}
+              className={`w-full h-full rounded-md relative bg-${pathname.slice(
+                1
+              )}${index}`}
             >
               <h2 className="absolute text-[30px] bottom-[100px] left-5">
                 {item.title}
